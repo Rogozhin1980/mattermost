cbc4678fd40f1c60d5f1c894c5c4da7a0048822c
lorumic <michele9.6.89@gmail.com>
2024-11-14 16:18:08 +0100

    Use standard CSS rules to resize emoticons

--- webapp/channels/build/emoji/make_emojis.mjs
+++ webapp/channels/build/emoji/make_emojis.mjs
@@ -443,9 +443,8 @@ const cssRules = `
     background-repeat: no-repeat;
     cursor: pointer;
     -moz-transform: scale(0.5);
+    transform: scale(0.5);
     transform-origin: 0 0;
-    // Using zoom for now as it results in less blurry emojis on Chrome - MM-34178
-    zoom: 0.5;
 }
 
 .emojisprite {
@@ -456,7 +455,7 @@ const cssRules = `
     border-radius: 18px;
     cursor: pointer;
     -moz-transform: scale(0.35);
-    zoom: 0.35;
+    transform: scale(0.35);
 }
 
 .emojisprite-loading {
@@ -468,7 +467,7 @@ const cssRules = `
     border-radius: 18px;
     cursor: pointer;
     -moz-transform: scale(0.35);
-    zoom: 0.35;
+    transform: scale(0.35);
 }
 
 ${cssCats.join('\n')}
